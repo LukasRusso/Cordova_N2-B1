5d5a692f3cbabe6f4944179cdab1c9c19815cc2b
Lucas Araujo Russo <lukas_russo_92@hotmail.com>
2021-03-17 22:36:31 -0300

    N2-B1 App Estrutura

---
+++ www/js/index.js
@@ -0,0 +1,23 @@
+function getCorpo(){
+    return document.getElementById("Corpo");
+}
+
+function News(){
+    var corpo = getCorpo();
+    corpo.innerHTML = "News";
+}
+
+function Music(){
+    var corpo = getCorpo();
+    corpo.innerHTML = "Music";    
+}
+
+function Games(){
+    var corpo = getCorpo();
+    corpo.innerHTML = "Games";    
+}
+
+function Books(){
+    var corpo = getCorpo();
+    corpo.innerHTML = "Books";    
+}
