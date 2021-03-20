1d2f1ee35067b50374fc27dbb0b0866c886f93e7
Lucas Araujo Russo <lukas_russo_92@hotmail.com>
2021-03-20 16:42:33 -0300

    Russo API - Finish

---
+++ www/Path/Russo/js/newsAPI.js
@@ -0,0 +1,186 @@
+function createCard(image, title, text, link){
+    var card = document.createElement("div");
+    card.className = "card";
+
+    var cardImg = document.createElement("img");
+    cardImg.className = "card-img-top";
+    cardImg.src = image;
+
+    var cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    var cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    cardTitle.innerHTML = title;
+    cardTitle.style = "font-weight: bold";
+
+    var cardText = document.createElement("p");
+    cardText.className = "card-text";
+    cardText.innerHTML = text;
+
+    var cardLink = document.createElement("a");
+    cardLink.className = "btn btn-primary";
+    cardLink.target = "_blank"
+    cardLink.innerHTML = "Go to the WebSite";
+    cardLink.href = link;
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(cardLink);
+
+    card.appendChild(cardImg);
+    card.appendChild(cardBody);
+
+    return card;
+}
+
+function getCardList(){
+    return document.getElementById("cardList");
+}
+
+function getArticles(){
+    const apiKey = "RxqU7wHepyxF3ADjIQqAmkVijsp7EJuu";
+
+    var cardList = getCardList();
+    cardList.innerHTML = "";
+
+    var htmlRoute = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    htmlRoute += "?sort=newest";
+    htmlRoute += "&api-key=" + apiKey;
+
+
+    $.ajax({url: htmlRoute, method: "GET"})
+        .done(function(response) {
+            console.log(response);
+            var data = response;
+
+            if(response.status == "OK"){
+                data.response.docs.forEach(element => {                    
+                    cardList.appendChild(
+                        createCard(
+                            "https://static01.nyt.com/" + element.multimedia[0].url,
+                            element.abstract,
+                            element.lead_paragraph,
+                            element.web_url
+                        )
+                    )
+                });
+            }
+        })       
+}
+
+function getBusiness(){
+    const apiKey = "RxqU7wHepyxF3ADjIQqAmkVijsp7EJuu";
+
+    var cardList = getCardList();
+    cardList.innerHTML = "";
+
+    var htmlRoute = "https://api.nytimes.com/svc/topstories/v2/business.json"; 
+    htmlRoute += "?api-key=" + apiKey;
+
+
+    $.ajax({url: htmlRoute, method: "GET"})
+        .done(function(response) {            
+            var data = response;
+
+            if(response.status == "OK"){
+                data.results.forEach(element => {                    
+                    cardList.appendChild(
+                        createCard(
+                            element.multimedia[0].url,
+                            element.title,
+                            element.abstract,
+                            element.web_url
+                        )
+                    )
+                });
+            }
+        })       
+}
+
+function getFood(){
+    const apiKey = "RxqU7wHepyxF3ADjIQqAmkVijsp7EJuu";
+
+    var cardList = getCardList();
+    cardList.innerHTML = "";
+
+    var htmlRoute = "https://api.nytimes.com/svc/topstories/v2/food.json"; 
+    htmlRoute += "?api-key=" + apiKey;
+
+
+    $.ajax({url: htmlRoute, method: "GET"})
+        .done(function(response) {            
+            var data = response;
+
+            if(response.status == "OK"){
+                data.results.forEach(element => {                    
+                    cardList.appendChild(
+                        createCard(
+                            element.multimedia[0].url,
+                            element.title,
+                            element.abstract,
+                            element.web_url
+                        )
+                    )
+                });
+            }
+        })      
+}
+
+function getHealth(){
+    const apiKey = "RxqU7wHepyxF3ADjIQqAmkVijsp7EJuu";
+
+    var cardList = getCardList();
+    cardList.innerHTML = "";
+
+    var htmlRoute = "https://api.nytimes.com/svc/topstories/v2/health.json"; 
+    htmlRoute += "?api-key=" + apiKey;
+
+
+    $.ajax({url: htmlRoute, method: "GET"})
+        .done(function(response) {            
+            var data = response;
+
+            if(response.status == "OK"){
+                data.results.forEach(element => {                    
+                    cardList.appendChild(
+                        createCard(
+                            element.multimedia[0].url,
+                            element.title,
+                            element.abstract,
+                            element.web_url
+                        )
+                    )
+                });
+            }
+        })      
+}
+
+function getHome(){
+    const apiKey = "RxqU7wHepyxF3ADjIQqAmkVijsp7EJuu";
+
+    var cardList = getCardList();
+    cardList.innerHTML = "";
+
+    var htmlRoute = "https://api.nytimes.com/svc/topstories/v2/home.json"; 
+    htmlRoute += "?api-key=" + apiKey;
+
+
+    $.ajax({url: htmlRoute, method: "GET"})
+        .done(function(response) {            
+            var data = response;
+
+            if(response.status == "OK"){
+                data.results.forEach(element => {                    
+                    cardList.appendChild(
+                        createCard(
+                            element.multimedia[0].url,
+                            element.title,
+                            element.abstract,
+                            element.web_url
+                        )
+                    )
+                });
+            }
+        })         
+}
--- www/js/index.js
+++ www/js/index.js
@@ -2,22 +2,61 @@ function getCorpo(){
     return document.getElementById("Corpo");
 }
 
+function getList(){
+    return document.getElementById("list");
+}
+
 function News(){
-    var corpo = getCorpo();
-    corpo.innerHTML = "News";
+    var list = getList()
+    list.innerHTML = "";
+    var corpo = getCorpo();    
+    corpo.innerHTML = "";
+
+    $.ajax("./Path/Russo/news.html").done(function(data) {
+        $("#Corpo").html(data);
+    });
+    
+    var logo = document.getElementById("logo");
+    logo.setAttribute("src", "./img/NYT_logo.png");    
+
+    var routes = ["Articles", "Business","Food", "Health", "Home"];
+
+    routes.forEach(element => {
+        var li = document.createElement("li");        
+        var a = document.createElement("a");
+        a.setAttribute("onclick", "get" + element + "()");
+        a.innerHTML = element;
+        a.href = "#";
+
+        li.appendChild(a);
+        list.appendChild(li);
+    })
 }
 
 function Music(){
+    var list = getList()
+    list.innerHTML = "";
     var corpo = getCorpo();
-    corpo.innerHTML = "Music";    
+    corpo.innerHTML = "";
+
+    corpo.innerHTML = "Music";  
+
 }
 
 function Games(){
+    var list = getList()
+    list.innerHTML = "";
     var corpo = getCorpo();
+    corpo.innerHTML = "";
+
     corpo.innerHTML = "Games";    
 }
 
 function Books(){
+    var list = getList()
+    list.innerHTML = "";
     var corpo = getCorpo();
+    corpo.innerHTML = "";
+
     corpo.innerHTML = "Books";    
 }
